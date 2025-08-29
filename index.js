956085dd3a7c03a8b8c3d238ac4e8c34eed3761a
Васкевич Олег <vaskevich@jukola.office>
2025-08-29 18:24:28 +0300

    refactor: clean up code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { xml2js, js2xml } from 'xml-js';
 import { readFileSync, writeFileSync } from 'fs';
-import { readdir } from 'node:fs/promises';
+import { readdir, readFile, writeFile } from 'node:fs/promises';
 import path from 'node:path';
 
 
@@ -23,45 +23,87 @@ function getCatalogSetOfProperties(){
 }
 
 function isCatalog(properties, processedCatalogs) {
+  return isTargetObject(properties, processedCatalogs, 'Catalog');
+}
+
+function isDocument(properties, processedDocuments) {
+  return isTargetObject(properties, processedDocuments, 'Document');
+}
+
+function isTargetObject(properties, processedObjects, objectName) {
  
   const props = properties.filter(el => el.name === 'name' && el.hasOwnProperty('elements') && el.elements.length > 0);
 
   if (props.length) {
-      const propElements = props[0].elements;
+    const propElements = props[0].elements;
 
-      const a = propElements
-        .filter(el => el.hasOwnProperty('text') && String(el.text).startsWith('Catalog.'));
+    const a = propElements
+      .filter(el => el.hasOwnProperty('text') && String(el.text).startsWith(`${objectName}.`));
 
-      if (a.length > 0) {
-        processedCatalogs.add(String(a[0].text).split('.')[1]);
+    if (a.length > 0) {
+      const arr = String(a[0].text).split('.');
+
+      // 1. Document.Invoice.StandardAttribute.Description
+      // 2. Document.Invoice
+      // we want to add only 2 into processed objects, also only 2 qualifies as the target object 
+      if (arr.length === 2) {
+        processedObjects.add(arr[1]);
+        return true;
       }
-
-      return a.length > 0;
+    }
   }
 
   return false;
  
 }
 
-function isDocument(properties, processedDocuments) {
- 
-  const props = properties.filter(el => el.name === 'name' && el.hasOwnProperty('elements') && el.elements.length > 0);
-
-  if (props.length) {
-      const propElements = props[0].elements;
-
-      const a = propElements
-        .filter(el => el.hasOwnProperty('text') && String(el.text).startsWith('Document.'));
-
-      if (a.length > 0) {
-        processedDocuments.add(String(a[0].text).split('.')[1])
-      }
-
-      return a.length > 0;
+function newRightElement(rightName) {
+  const rightElement = { 
+    type: 'element', 
+    name: 'right',  
+    elements: [
+      { type: 'element',
+        name: 'name', 
+        elements: [ 
+          { 
+          type: 'text', 
+          text: rightName
+          },
+        ],
+      },
+      { type: 'element', 
+        name: 'value', 
+        elements: [ 
+          { type: 'text', 
+            text: 'false' 
+          }, 
+        ],
+      },
+    ] 
   }
 
-  return false;
- 
+  return rightElement;
+}
+
+function newObjectElement(rootObjectName, objectName, rightElements = []) {
+  const objectElement = {
+    type: 'element', 
+    name: 'object', 
+    elements: [
+      { 
+        type: 'element', 
+        name: 'name', 
+        elements: [
+          { type: 'text', 
+            text: `${rootObjectName}.${objectName}`,
+          },
+        ],
+      },
+        ...rightElements,
+    ]
+  }
+
+
 }
 
 function editProperties(properties, setOfProperties) {
@@ -76,59 +118,40 @@ function editProperties(properties, setOfProperties) {
   props.forEach((el) => {
     const rightProps = el.elements;
     const rightPropsName = rightProps.find(k => k.hasOwnProperty('name') && k.name === 'name');
-    const rightPropsVlue = rightProps.find(k => k.hasOwnProperty('name') && k.name === 'value');
+    const rightPropsValue = rightProps.find(k => k.hasOwnProperty('name') && k.name === 'value');
 
     if (rightPropsName.hasOwnProperty('elements') && rightPropsName.elements.length && setOfProperties.has(rightPropsName.elements[0].text)) {
-      if (rightPropsVlue.hasOwnProperty('elements') && rightPropsVlue.elements.length) {
-        rightPropsVlue.elements[0].text = 'false';
+      if (rightPropsValue.hasOwnProperty('elements') && rightPropsValue.elements.length) {
+        rightPropsValue.elements[0].text = 'false';
         processedProps.add(rightPropsName.elements[0].text);
       }
-    }
-    
+    } 
   });
 
   setOfProperties.keys().forEach((key) => {
     if (!processedProps.has(key)) {
-      properties.push({ type: 'element', name: 'right',  elements: [
-        { type: 'element', name: 'name', elements: [ { type: 'text', text: key} ]},
-        { type: 'element', name: 'value', elements: [ { type: 'text', text: 'false' } ]}
-      ] });
+      properties.push(newRightElement(key));
     }
   });
 
 }
 
-function addUnprocessedObjects(RighstElements, listOfObjects, processedObjects, setOfProperties, objectName) {
-  listOfObjects.forEach((doc) => {
-    if (!processedObjects.has(doc)) {
+function addUnprocessedObjects(objectElements, listOfObjects, processedObjects, setOfProperties, rootObjectName) {
+  listOfObjects.forEach((objectName) => {
+    if (!processedObjects.has(objectName)) {
+      const rightElements = [];
 
-      const setOfRights = [];
       setOfProperties.keys().forEach((key) => {
-        setOfRights.push({ type: 'element', name: 'right',  elements: [
-          { type: 'element', name: 'name', elements: [ { type: 'text', text: key} ]},
-          { type: 'element', name: 'value', elements: [ { type: 'text', text: 'false' } ]}
-        ]});
+        rightElements.push(newRightElement(key));
       });
 
-
-      const objectRight = {
-        type: 'element', name: 'object', elements: [
-          { 
-            type: 'element', name: 'name', elements: [
-            { type: 'text', text: `${objectName}.${doc}`}
-          ]},
-          ...setOfRights,
-        ]
-      }
-
-      RighstElements.push(objectRight);
-
+      objectElements.push(newObjectElement(rootObjectName, objectName, rightElements));
     }
-  })  
+  });  
 }
 
-function processFile(filePath, documents, catalogs) {
-  const xml = readFileSync(filePath, 'utf8');
+async function processFile(filePath, documents, catalogs) {
+  const xml = await readFile(filePath, 'utf8');
 
   const result = xml2js(xml, {compact: false, spaces: 2});
 
@@ -187,7 +210,7 @@ function processFile(filePath, documents, catalogs) {
 
   const xmlres = js2xml(result, {compact: false, spaces: 2});
 
-  writeFileSync(filePath, xmlres);
+  await writeFile(filePath, xmlres);
 }
 
 function getDirPath() {
@@ -196,30 +219,51 @@ function getDirPath() {
   return args[0]?.split('=')[1] || undefined;
 };
 
-async function getFilePaths(dirPath) {
-  try {
-    const rolesDirPath = path.join(dirPath, 'Roles\\');
-    const documentsDirPath = path.join(dirPath, 'Documents\\');
-    const catalogsDirPath = path.join(dirPath, 'Catalogs\\');
 
+async function getListOfObjects(dirPath, folderName) {
+
+  const documentsDirPath = path.join(dirPath, `${folderName}\\`);
+
+  try {
     const documentFiles = await readdir(documentsDirPath);
 
     const documents = documentFiles
       .filter((docFile) => path.extname(docFile) === '.xml')
       .map((docFile) => path.basename(docFile, '.xml'));
 
-    const catalogFiles = await readdir(catalogsDirPath);
+    return documents;
+    
+  } catch (error) {
+    console.error(err);
+  }
 
-    const catalogs = catalogFiles
-      .filter((catFile) => path.extname(catFile) === '.xml')
-      .map((catFile) => path.basename(catFile, '.xml'));
+  return [];    
+}
 
-    const files = await readdir(rolesDirPath, { recursive: true });
-    for (const file of files) {
+async function getListOfRoleFiles(dirPath) {
+  try {
+    const files = await readdir(dirPath, { recursive: true }); 
+
+    return files;
+  } catch (error) {
+    console.error(err);
+  }
+
+  return [];
+}
+
+async function processConfigFiles(dirPath) {
+  try {
+    const rolesDirPath = path.join(dirPath, 'Roles\\');
+
+    const documents = await getListOfObjects(dirPath, 'Documents');
+    const catalogs = await getListOfObjects(dirPath, 'Catalogs'); 
+    const rolefiles = await getListOfRoleFiles(rolesDirPath);
+
+    for (const file of rolefiles) {
       if (path.basename(file) === 'Rights.xml') {
-
         const filePath = path.join(rolesDirPath, path.dirname(file), path.basename(file));
-        processFile(filePath, documents, catalogs); 
+        await processFile(filePath, documents, catalogs); 
       }
     }
   } catch (err) {
@@ -227,5 +271,5 @@ async function getFilePaths(dirPath) {
   } 
 }
 
-getFilePaths(getDirPath());
+processConfigFiles(getDirPath());
 
